e58bc41c2733bd7aca3ecc30ab5341f81adb503b
alexandravdeenko <alex-avdeenko@yandex.ru>
2022-09-18 15:39:38 +0700

    Сделана первая задача найти максимальное число

---
+++ Ex_001/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 5;
+int b = 7;
+int c = 3;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("c = " + c);
+Console.WriteLine("max = " + max);
